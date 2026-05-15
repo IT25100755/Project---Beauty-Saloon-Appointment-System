a8d93be80d13afae8af690e06f02177d5417a869
IT25103447 <it25103447@my.sliit.lk>
2026-05-15 18:59:19 +0530

    Update ProductRepository.java

--- src/main/java/com/Booking/Booking/System/repository/ProductRepository.java
+++ src/main/java/com/Booking/Booking/System/repository/ProductRepository.java
@@ -4,10 +4,6 @@ import com.Booking.Booking.System.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repository interface for Product.
- * Inherits full CRUD from JpaRepository.
- */
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
