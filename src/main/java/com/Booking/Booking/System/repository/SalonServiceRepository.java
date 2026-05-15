4d6736c171460b51b8f9157dccc0079a59f4adab
IT25103405 <it25103405@my.sliit.lk>
2026-05-15 18:55:58 +0530

    Update SalonServiceRepository.java

--- src/main/java/com/Booking/Booking/System/repository/SalonServiceRepository.java
+++ src/main/java/com/Booking/Booking/System/repository/SalonServiceRepository.java
@@ -4,10 +4,6 @@ import com.Booking.Booking.System.model.SalonService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repository interface for SalonService.
- * Inherits full CRUD from JpaRepository.
- */
 @Repository
 public interface SalonServiceRepository extends JpaRepository<SalonService, Long> {
 }
