beefec1df435fc78fac41645dd9cca9668a64ea8
IT25101495 <IT25101495@my.sliit.lk>
2026-05-19 21:22:17 +0530

    Delete src/main/java/com/Booking/Booking/System/repository/AdminUserRepository.java

--- src/main/java/com/Booking/Booking/System/repository/AdminUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Booking.Booking.System.repository;
-
-import com.Booking.Booking.System.model.AdminUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
-}
